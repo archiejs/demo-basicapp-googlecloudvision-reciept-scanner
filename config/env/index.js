ed0b4a16b30d498477e8d333473555f179da2677
Naval <navalnovel@gmail.com>
2016-06-05 22:16:40 +0530

    refactor in config file

--- config/env/index.js
+++ config/env/index.js
@@ -2,18 +2,18 @@ var _ = require('lodash'),
     path = require('path');
 
 // Get the current config
-var envFile = process.env.NODE_ENV || 'local'; // default
-var environmentConfig = require(path.join(process.cwd(), 'config', 'env', envFile)) || {};
+var envFilename = process.env.NODE_ENV || 'local'; // default
+var envConfig = require('./' + envFilename) || {};
 
 // Get the default config
 var defaultConfig = require('./default');
 
 // Merge config files
-var envConfig = _.merge(defaultConfig, environmentConfig);
+var appConfig = _.merge(defaultConfig, envConfig);
 
 // Get files (ex. routes)
-envConfig.files = {
+appConfig.files = {
     routes: require('./../routes')
 };
 
-module.exports = envConfig;
+module.exports = appConfig;
