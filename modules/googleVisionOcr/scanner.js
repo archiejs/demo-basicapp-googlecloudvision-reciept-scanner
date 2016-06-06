8fc0afb736d1cc5799c8c302b1231e50948599bc
Naval <navalnovel@gmail.com>
2016-06-06 21:29:40 +0530

    added a rubimentary algo to find amount on a reciept

--- modules/googleVisionOcr/scanner.js
+++ modules/googleVisionOcr/scanner.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//https://github.com/GoogleCloudPlatform/nodejs-docs-samples/blob/master/vision/labelDetection.js
-
 const GCloud = require('gcloud');
 
 var ScanDoc = function(options) {
@@ -9,15 +7,38 @@ var ScanDoc = function(options) {
   this.vision = gcloud.vision();
 }
 
-ScanDoc.prototype.detectLabels = function(inputFile, callback) {
+ScanDoc.prototype.detectAmountInRecipt = function(inputFile, callback) {
   // Make a call to the Vision API to detect the labels
-  this.vision.detectLabels(inputFile, { verbose: true }, function (err, labels) {
-    if (err) {
-      return callback(err);
+  this.vision.detectText(inputFile, { verbose: true },
+    function(err, result){
+      if(err) {
+        return callback(err);
+      }
+      
+      var desc = result[0].desc;
+      var amts = 
+        desc
+        .split(/[\n ,]+/)
+        .filter( function(amt) {
+          return amt.match("[0-9]\\.[0-9][0-9]$") != null; // all amounts end with .XX
+        })
+        .map( function(amt) {
+          return parseFloat(amt);  // convert string to float
+        });
+
+      var sorted = amts.sort(function(a,b) { return a-b; }); // asc
+      var length = sorted.length-1; // max (usually the bill amount)
+      var start = 0; // min
+
+      if(sorted[start] < 0) {
+        length--; // choose second largest value
+      }
+
+      var amount = sorted[length];
+
+      return callback(null, result, amount);
     }
-    console.log('result:', JSON.stringify(labels, null, 2));
-    callback(null, labels);
-  });
+  );
 }
 
 module.exports = ScanDoc;
--- modules/googleVisionOcr/test/scanner.js
+++ modules/googleVisionOcr/test/scanner.js
@@ -19,11 +19,23 @@ describe('Plugin cloudvisionOcr Testcases:', function(){
     });
   });
 
-  afterEach(function(){
+  it('#test faded pink recipt', function(done){
+    scanner.detectAmountInRecipt('./test/image1.jpg', function(err, result, amount){
+      if(err) return done(err);
+      should.exist(result, "Result should exist");
+      amount.should.equal(38.00, "Incorrect amount");
+      console.log('result:', JSON.stringify(result[0].desc, null, 2));
+      done();
+    });
   });
 
-  it('#tests something', function(done){
-    scanner.detectLabels('./test/image2.jpg', done);
+  it('#test half recipt', function(done){
+    scanner.detectAmountInRecipt('./test/image2.jpg', function(err, result, amount){
+      if (err) return done(err);
+      should.exist(result, "Result should exist");
+      amount.should.equal(184.00, "Incorrect amount");
+      console.log('result:', JSON.stringify(result[0].desc, null, 2));
+      done();
+    });
   });
-
 });
