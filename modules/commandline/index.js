1e05aae7803a0f4f585f2c03715f8c578d735880
Naval <navalnovel@gmail.com>
2016-06-07 07:00:06 +0530

    renamed commandUtil to commandline

---
+++ modules/commandline/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var setup = function(options, imports) {
+  const drive = imports.GoogleDrive;
+  const scanner = imports.ScanReceipt;
+
+  // 1. Get the token
+  // 2. Get the drive directory
+  // 3. Prepare list of files to scan
+  // 4. Prepare an output.txt in the same folder
+};
+
+function getTokenFromUser() {
+}
+
+function getTaskDetailsFromUser() {
+}
+
+function getFilesFromDrive() {
+}
+
+function doScanNewReciepts() {
+}
+
+function doPrepareReport() {
+}
