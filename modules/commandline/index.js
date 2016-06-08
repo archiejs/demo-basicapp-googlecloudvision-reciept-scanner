dd1718f40bc1a1246ec7c4842673f0d9791be183
Naval <navalnovel@gmail.com>
2016-06-08 08:07:32 +0530

    intermediate commit for commandline module

--- modules/commandline/index.js
+++ modules/commandline/index.js
@@ -1,8 +1,21 @@
 'use strict';
 
+var readline = require('readline');
+
+var drive;
+var scanner;
+var googAuthToken;
+var auth;
+var readline;
+
 var setup = function(options, imports) {
-  const drive = imports.GoogleDrive;
-  const scanner = imports.ScanReceipt;
+  drive = imports.GoogleDrive;
+  scanner = imports.ScanReceipt;
+  googAuthToken = imports.googleAuthToken;
+  readline = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
   // 1. Get the token
   // 2. Get the drive directory
@@ -10,10 +23,32 @@ var setup = function(options, imports) {
   // 4. Prepare an output.txt in the same folder
 };
 
-function getTokenFromUser() {
+function getTokenFromUser(done) {
+  googAuthToken(function(_auth){
+    auth = _auth;
+    done();
+  });
 }
 
-function getTaskDetailsFromUser() {
+function getTaskDetailsFromUser(done) {
+  var data = {};
+  new Promise()
+    .then(function(done, fail) {
+      readline.question('What is the month on the reciepts (1-12) : ', function(month) {
+        if(month) {
+          data.month = month;
+        }
+        done();
+      });
+    })
+    .then(function(done, fail) {
+      readline.question('What is the year on reciepts (YY) : ', function(year) {
+        if(year) {
+          data.year = year;
+        }
+        done();
+      });
+    });
 }
 
 function getFilesFromDrive() {
